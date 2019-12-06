1354a995d85f08775698da3fc728a6721b097f64
Huseyn Yusifov <55439613+HuseynF@users.noreply.github.com>
2019-12-06 16:16:41 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Phone.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp1
+{
+    class Phone
+    {
+        public Phone(string Color, string Name,string Country)
+        {
+            color = Color;
+            name = Name;
+            country = Country;
+        }
+        public string color;
+        public string name;
+        public string country;
+        private long _Imei;
+
+        public void SetImei(long Number)
+        {
+            if (Number.ToString().Length >15 || Number < 0)
+            {
+                System.Console.WriteLine("Please write correctly"); 
+            }
+            else
+            {
+                _Imei = Number;
+            }
+
+    
+        }   
+        public long GetImei()
+        {
+            return _Imei;
+        }
+        public void Call(long num)
+        {
+            if(num.ToString().Length<9 || num > 0)
+            {
+                System.Console.WriteLine(num +" "+ " now calling");
+            
+            }
+            else {
+                System.Console.WriteLine("Wrong");
+            }
+            
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone Apple = new Phone("gold","iphonexs","USA");
+            Apple.SetImei(123456789123456);
+            Console.WriteLine(Apple.GetImei());
+            Apple.Call(994502059999);
+        }
+    }
+}
